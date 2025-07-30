7117487d1c1d30e76eb783c5fed843b15ed29179
UmeshaJayakody <umesha.22@cse.mrt.ac.lk>
2025-07-30 08:16:46 +0530

    refactor: streamline auth middleware and reintroduce it in user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,7 +3,7 @@ const router = Router();
 import { createUser,loginUser,getUserProfile } from '../controllers/user.controller.js';
 import validate from '../middlewares/validation.middleware.js';
 import { registerSchema,loginSchema } from '../validators/user.validator.js';
-// import authMiddleware from '../middlewares/auth.middleware.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
 
 router.post('/register', validate(registerSchema), createUser);
 router.post('/login', validate(loginSchema), loginUser);
