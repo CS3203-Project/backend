e348316e4280f23042a046ecd520fb9781ec660c
indunil-k <indunilwak.22@uom.lk>
2025-08-10 18:18:13 +0530

    added userSearch api for chat purposes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 const router = Router();
-import { createUser,loginUser,getUserProfile,updateUserProfile,deleteUserProfile,checkEmailExists} from '../controllers/user.controller.js';
+import { createUser,loginUser,getUserProfile,updateUserProfile,deleteUserProfile,checkEmailExistsController,searchUsersController} from '../controllers/user.controller.js';
 import validate from '../middlewares/validation.middleware.js';
 import { registerSchema,loginSchema,updateProfileSchema } from '../validators/user.validator.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 
-router.get('/check-email', checkEmailExists);
+router.get('/check-email', checkEmailExistsController);
+router.get('/search', authMiddleware, searchUsersController);
 router.post('/register', validate(registerSchema), createUser);
 router.post('/login', validate(loginSchema), loginUser);
 router.get('/profile', authMiddleware, getUserProfile);
