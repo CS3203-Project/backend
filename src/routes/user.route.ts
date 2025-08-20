26851f91d33fbf6000a52b858edb7749b968caf4
UmeshaJayakody <jayakodyjauc.22@uom.lk>
2025-08-20 18:56:29 +0530

    feat: add upload image endpoint with S3 integration

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 const router = Router();
-import { createUser,loginUser,getUserProfile,updateUserProfile,deleteUserProfile,checkEmailExistsController,searchUsersController} from '../controllers/user.controller.js';
+import { createUser,loginUser,getUserProfile,updateUserProfile,deleteUserProfile,checkEmailExistsController,searchUsersController,uploadImageController} from '../controllers/user.controller.js';
 import validate from '../middlewares/validation.middleware.js';
 import { registerSchema,loginSchema,updateProfileSchema } from '../validators/user.validator.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
@@ -13,5 +13,6 @@ router.post('/login', validate(loginSchema), loginUser);
 router.get('/profile', authMiddleware, getUserProfile);
 router.put('/profile', authMiddleware, upload.single('profileImage'), updateUserProfile);
 router.delete('/profile', authMiddleware, deleteUserProfile);
+router.post('/upload-image', authMiddleware, upload.single('image'), uploadImageController);
 
 export default router;
