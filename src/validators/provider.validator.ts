c0c3128134f377525abc92dec3c9cb7136835aa8
indunil-k <indunilwak.22@uom.lk>
2025-09-04 17:02:30 +0530

    provider form error resolved

--- src/validators/provider.validator.ts
+++ src/validators/provider.validator.ts
@@ -4,16 +4,16 @@ export const createProviderSchema = Joi.object({
   bio: Joi.string().max(1000).optional(),
   skills: Joi.array().items(Joi.string()).optional(),
   qualifications: Joi.array().items(Joi.string()).optional(),
-  logoUrl: Joi.string().uri().optional(),
-  IDCardUrl: Joi.string().uri().required() // Required ID card image URL
+  logoUrl: Joi.string().uri().allow('').optional(),
+  IDCardUrl: Joi.string().uri().allow('').optional() // Optional ID card image URL
 });
 
 export const updateProviderSchema = Joi.object({
   bio: Joi.string().max(1000).optional(),
   skills: Joi.array().items(Joi.string()).optional(),
   qualifications: Joi.array().items(Joi.string()).optional(),
-  logoUrl: Joi.string().uri().optional(),
-  IDCardUrl: Joi.string().uri().optional() // Optional for updates
+  logoUrl: Joi.string().uri().allow('').optional(),
+  IDCardUrl: Joi.string().uri().allow('').optional() // Optional for updates
 });
 
 export const providerParamsSchema = Joi.object({
